b4906ba38e8d813323813c11a04fadb956977def
henjimantaring <henji.mantaring@gmail.com>
2025-11-23 22:51:03 +0800

    channel

--- run.sh
+++ run.sh
@@ -20,3 +20,4 @@ ajv validate -s /home/bevel/platforms/network-schema.json -d /home/bevel/build/n
 
 echo "Running the playbook..."
 exec ansible-playbook -vvv /home/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/bevel/platforms/shared/inventory/ -e "@/home/bevel/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
+ansible-playbook -vvv platforms/hyperledger-fabric/configuration/add-new-channel.yaml --extra-vars "@build/network.yaml" --inventory-file platforms/shared/inventory/ansible_provisioners -e 'ansible_python_interpreter=/usr/bin/python3'
